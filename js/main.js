c340ce31db5e142c0a24a8bf7232cf3a9dcfe0b1
Sigrid Stensvold <sigridstens@gmail.com>
2018-12-06 09:19:28 -0600

    Added javascript toggle to modal nav styled header nav

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
 var input = document.querySelectorAll('input');
 for(i=0; i<input.length; i++){
-  input[i].setAttribute('size',input[i].getAttribute('placeholder').length);
+  if (input[i].getAttribute("placeholder") !== null) {
+    input[i].setAttribute('size',input[i].getAttribute('placeholder').length);
+  }
 }
+
+var navButton = document.querySelector('.nav-icon');
+var modalNav = document.querySelector('.modal-nav');
+
+navButton.addEventListener('click', function() {
+  console.log('open sesame!!!');
+
+  if (modalNav !== null) {
+    modalNav.style.display = "block";
+  }
+
+});
+
+var closeButton = document.querySelector('.close-icon');
+
+closeButton.addEventListener('click', function() {
+  console.log('close it!');
+  
+  if (modalNav !== null) {
+    modalNav.style.display = "none";
+  }
+
+});
